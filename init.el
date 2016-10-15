6a8f81e9eabeec9474f668cb69c218c01d2a232b
Robert J. Berger <rberger@ibd.com>
2016-10-15 14:02:49 -0700

    Some customization for paredit/smartparens

--- init.el
+++ init.el
@@ -340,3 +340,15 @@ you should place your code here."
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(sp-base-key-bindings (quote paredit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
